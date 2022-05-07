76d2c2249d070ddff4675c3757b1d7d058e21520
{K.M_UGOSYSTEM} <{sdanggaki.cw@gmail.com}>
2022-05-08 07:07:34 +0900

    scrapeing code update

--- scraping.py
+++ scraping.py
@@ -19,11 +19,17 @@ def wagatomo_scrape():
     wagatomo_url_list = [atag.get('href') for atag in atags if 'article' in atag.get('href')]
     wagatomo_cont = get_content_by_url(url=f'https:{wagatomo_url_list[0]}')
     wagatomo_div = wagatomo_cont.find(class_='phase2_outer clearfix')
-    date_span = wagatomo_div.find(class_='ts_days')
+    wagatomo_spans = wagatomo_div.find_all('span')
+    date_span =0
+    title_span = [span.text for span in wagatomo_spans if '今週のことば' in span.text or 'わが友' in span.text]
+    date_span = [span.text for span in wagatomo_spans if 'ts_days' in span.get('class')]
+    title = title_span[0] if len(title_span) > 0 else ''
+    to_day = date_span[0] if len(date_span) > 0 else ''
     wagatomo_p = wagatomo_div.find_all('p')
     texts:list[str] = [p.text for p in wagatomo_p]
     parse_text = texts[0].replace(' ','').replace('\u3000','\r\n\u3000')
-    result_text = f'わが友に贈る　{date_span.text}{parse_text}'
+    parse_title = title.replace(' ','').replace('\r\n','')
+    result_text = f'{parse_title}　{to_day}{parse_text}'
     return result_text
 
 # 
