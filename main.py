311a5b219905d718a7fe042b4af9540085947ad9
{K.M_UGOSYSTEM} <{sdanggaki.cw@gmail.com}>
2024-04-26 00:05:56 +0900

    developer keyword add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-from lib import json , os
+from lib import json , os , re
 from flask import Flask , request ,abort
  
 # Line SDK ---------------------
@@ -29,6 +29,8 @@ app = Flask(__name__)
 CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
 DEVELOPER_USER_ID = os.getenv("DEVELOPER_USER_ID")
 CHANNEL_SECRET = os.getenv('CHANNEL_SECRET')
+
+DEVELOPER_KEYWORD = os.getenv('DEVELOPER_KEYWORD')
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
@@ -70,14 +72,58 @@ def say_michael():
 
 @handler.add(FollowEvent)
 def follow_event_hundler(event:FollowEvent):
+    # thread_id
     U = User(user_id=event.source.user_id)
+    th = ai.create_empty_thread()
+    U.openai_data.thread_id = th.id
+    U.update_firestore_doc()
+    
+    # 自己紹介
+    ai.create_thread_message(
+        thread_id=th.id,
+        role='user',
+        content='自己紹介してください'
+    )
+
+    # run
+    result = ai.create_thread_run(
+        thread_id=th.id,
+        assistant_id=ai.OPENAI_ASS_ID
+    )
+    msg = result.data[0].content[0].text.value
+
+    line_bot_api.push_message(
+        to=U.id,
+        messages=TextSendMessage(text=msg)
+    )
+
+@handler.add(UnfollowEvent)
+def unfollow_event_hundler(event:UnfollowEvent):
+    U = User(user_id=event.source.user_id)
+    del_res = ai.delete_thread(thread_id=U.openai_data.thread_id)
+    U.openai_data.thread_id = None
+    U.update_firestore_doc()
+    print(del_res)
 
 
 @handler.add(MessageEvent,message=TextMessage)
 def text_message_handler(event:MessageEvent):
 
-    U = User(user_id=event.source.user_id)
+    rgex = re.compile(r''+DEVELOPER_KEYWORD+'*')
+    mt = rgex.match(event.message.text)
+    if mt:
+        spl = ':'.split(mt[0])
+        ass = ai.get_assistant(assistant_id=ai.OPENAI_ASS_ID)
+        instructions = ass.instructions + spl[1]
+        ass_upd = ai.update_assistant(assistant_id=ass.id,instructions=instructions)
+        print(ass_upd)
+        line_bot_api.reply_message(
+            reply_token=event.reply_token,
+            messages=TextSendMessage(text='assistant_instructions:\n' + instructions)
+        )
+        return 'OK'
 
+    U = User(user_id=event.source.user_id)
     # スレッドの有無を確認
     if U.openai_data.thread_id is None:
         # create thread
@@ -107,9 +153,7 @@ def text_message_handler(event:MessageEvent):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=msg))
-    
-    
-
+    return 'OK'
 
  
 if __name__ == "__main__":
